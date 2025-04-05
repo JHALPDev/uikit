003a7cb99ceb368c8436d17d3e84d989fe9325f7
Jose Alcarraz Palomino <jose.alcarrazpalo@gmail.com>
2025-04-05 19:15:07 +0200

    feat: Setup Gradle and add basic UI composable
    - Removes the existing `.idea/uikit.iml` file.
    - Adds Gradle wrapper files (`gradle/wrapper/gradle-wrapper.properties`, `gradlew`, `gradlew.bat`) for consistent build execution.
    - Creates `gradle.properties` to define project-wide settings, including JVM arguments and AndroidX usage.
    - Adds `settings.gradle.kts` to manage the project structure and repository settings.
    - Introduces `gradle/libs.versions.toml` for centralized dependency management.
    - Creates `src/debug/kotlin/com/taycode/uikit/MainActivity.kt` to demonstrate a basic UI component.
    - Modifies `.idea/misc.xml` and `.idea/modules.xml` for updated project settings.
    - Adds a basic UI composable component (`MinimalInfoCard`).

---
+++ src/debug/kotlin/com/taycode/uikit/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.taycode.uikit
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.taycode.uikit.composables.cards.MinimalInfoCard
+import com.taycode.uikit.theme.UiKitTheme
+import org.koin.compose.KoinApplication
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UiKitTheme {
+                MinimalInfoCard(
+                    modifier = Modifier.fillMaxSize(),
+                    title = "Title",
+                    subtitle = "Subtitle"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MinimalInfoCardPreview() {
+    UiKitTheme {
+        MinimalInfoCard(
+            modifier = Modifier.fillMaxSize(),
+            title = "Title",
+            subtitle = "Subtitle"
+        )
+    }
+}
