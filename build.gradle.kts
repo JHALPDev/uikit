93046485344ae898718662212e657ae5c8d85482
Jose Alcarraz Palomino <jose.alcarrazpalo@gmail.com>
2025-04-11 01:13:29 +0200

    feat: Add KSP support and update dependencies
    - Adds the Kotlin Symbol Processing (KSP) plugin to the project.
    - Updates `gradle/libs.versions.toml` to include `ksp` version and `ksp` plugin definition.
    - Adds `ksp` plugin to `build.gradle.kts` for top level.
    - Adds `androidx.room.runtime` dependency and `androidx.room.compiler` with KSP support.
    - Updates `build.gradle.kts` to apply KSP plugin and include Room dependencies.
    - Adds `androidx.material3` and `koin.androidx.compose` dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)//Anche a livello superiore
 }
 
 android {
@@ -49,6 +50,10 @@ dependencies {
     api(libs.androidx.material3)
     api(libs.koin.androidx.compose)
 
+    api(libs.androidx.room.runtime)
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    ksp(libs.androidx.room.compiler)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
